2a60445503629fa8bc7707c14d257288c0f29d87
ChroniclerDelta <TheChroniclerDelta@outlook.com>
2024-10-26 19:33:43 -0500

    Began populating IEyelessDog.cs
    - Initial methods and fields created for Eyeless Dog states and actions
    - Only basic implementation done in EyelessDogPatches.cs
    - Will finish populating IEyelessDog.cs

--- src/ContentLib.API/Model/Entity/Enemy/Vanilla/EyelessDog/IEyelessDog.cs
+++ src/ContentLib.API/Model/Entity/Enemy/Vanilla/EyelessDog/IEyelessDog.cs
@@ -1,11 +1,39 @@
+using ContentLib.Core.Model.Entity.Util;
 using ContentLib.EnemyAPI.Model.Enemy;
+using UnityEngine;
 
 namespace ContentLib.API.Model.Entity.Enemy.Vanilla.EyelessDog;
 
 /// <summary>
 /// Interface representing the general functionality of the Eyeless Dog Vanilla Enemy. 
 /// </summary>
-public interface IEyelessDog : IEnemy
+public interface IEyelessDog : IEnemy, IKillable
 {
+    #region Fields
+
+    /// <summary>
+    /// Boolean representing whether this Eyeless Dog is lunging or not.
+    /// </summary>
+    bool isLunging { get; }
+    int suspicionLevel { get; }
+    Vector3 GuessedSearchPosition { get; }
+    Vector3 AbsoluteSearchPosition { get; }
+
+
+    #endregion
     
+    #region actions
+
+    void Lunge();
+    void GrabDeadBody(DeadBodyInfo body);
+    void DropDeadBody();
+
+    void AlertOtherDogs();
+
+
+    #endregion
+
+    #region suspicion
+
+    #endregion
 }
