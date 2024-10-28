5ba30804e73a2fa1a120ff106cecd3c3582dad72
RazielMartelus96 <razielmartelus@curiosity-core.com>
2024-10-28 00:59:00 +0000

    - Added the initial IPlayer.cs and populated with some appropriate fields. - Created and populated IHealable.cs & IRevivable.cs, util interfaces that may be useable in other IGameEntities in the future. - Next commit will focus on initial patch attempt to implement IPlayer.cs.

---
+++ src/ContentLib.API/Model/Entity/Player/IPlayer.cs
@@ -0,0 +1,26 @@
+using ContentLib.API.Model.Entity.Util;
+using ContentLib.Core.Model.Entity.Util;
+
+namespace ContentLib.API.Model.Entity.Player;
+
+/// <summary>
+/// Interface that represents the general functionality of a Player within the game space.
+/// </summary>
+public interface IPlayer :IGameEntity, IKillable,IRevivable, IHealable, ITeleportable
+{
+    /// <summary>
+    /// The stamina of the Player, when at 0, the Player is unable to run. 
+    /// </summary>
+    float Stamina { get; set; }
+    
+    /// <summary>
+    /// The amount of items the Player is currently holding within their inventory. 
+    /// </summary>
+    int HeldItemsAmount { get; }
+    
+    /// <summary>
+    /// Check that defines if a player is currently on the Terminal within the ship. 
+    /// </summary>
+    bool OnTerminal { get; }
+    
+}
