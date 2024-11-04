742c4990bd25bb5ab9c74845053c0d8641f5390d
RazielMartelus96 <razielmartelus@curiosity-core.com>
2024-11-04 21:24:34 +0000

    - Further small edits, mainly just cleaning of patches.

--- src/ContentLib.EnemyAPI/Patches/PlayerPatches.cs
+++ src/ContentLib.EnemyAPI/Patches/PlayerPatches.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
 using ContentLib.API.Model.Entity.Player;
+using ContentLib.API.Model.Event;
 using ContentLib.Core.Utils;
+using ContentLib.EnemyAPI.Events;
 using IL.GameNetcodeStuff;
 using UnityEngine;
 using PlayerControllerB = GameNetcodeStuff.PlayerControllerB;
@@ -19,26 +22,47 @@ public class PlayerPatches
     {
         orig(self);
         IPlayer player = new BasePlayerInstance(self);
-
+        GameEventManager.Instance.Trigger(new BasePlayerSpawnEvent(player));
     }
 
     private class BasePlayerInstance(PlayerControllerB playerController) : IPlayer
     {
+        private ShipTeleporter Teleporter => TeleporterPatches.Instance.ShipTeleporter;
         public ulong Id => playerController.NetworkObjectId;
         public bool IsAlive => !playerController.isPlayerDead;
         public int Health => playerController.health;
         public Vector3 Position => playerController.serverPlayerPosition;
-        public void Kill() => throw new NotImplementedException(); //TODO needs more looking into as quite complex.
+        public void Kill(Vector3 bodyVelocity,
+            bool spawnBody = true,
+            CauseOfDeath causeOfDeath = CauseOfDeath.Unknown,
+            int deathAnimation = 0,
+            Vector3 positionOffset = default) => playerController.KillPlayer(bodyVelocity,spawnBody, causeOfDeath, 
+            deathAnimation, positionOffset);
 
         public void Revive(Vector3 revivePosition) => throw new NotImplementedException();
-        public void Teleport(Vector3 position) => throw new NotImplementedException();
+        public void Teleport(Vector3 pos, bool withRotation = false, float rot = 0.0f, bool allowInteractTrigger = false,
+            bool enableController = true) => playerController.TeleportPlayer(pos, withRotation, 
+            rot, allowInteractTrigger,enableController);
 
-        public void TeleportToShip() => throw new NotImplementedException(); //playerController.TeleportPlayer() <-- use later when params are determined. 
+        //TODO check this system later to see why its an IEn
+        public void TeleportToShip()
+        {
+            if (Teleporter == null)
+            {
+                CLLogger.Instance.Log("Teleporter is null, teleporting not executed.");
+                return;
+            }
+            PlayerControllerB currentPlayer = StartOfRound.Instance.mapScreen.targetedPlayer;
+            StartOfRound.Instance.mapScreen.targetedPlayer = playerController;
+            IEnumerator beamUpPlayer = Teleporter.beamUpPlayer();
+            playerController.StartCoroutine(beamUpPlayer);
+            StartOfRound.Instance.mapScreen.targetedPlayer = currentPlayer;
+        }
 
         public float Stamina
         {
             get => playerController.sprintMeter;
-            set => throw new NotImplementedException(); //TODO needs to be a decent logic as it cant go over max etc
+            set => playerController.sprintMeter = value;
         }
 
         public int HeldItemsAmount
@@ -60,5 +84,10 @@ public class PlayerPatches
         public void HealOverTime(int healAmount, float healDurationInSeconds) => throw new NotImplementedException();
 
         public void Heal(int healAmount) => playerController.health += healAmount;
+    }
+
+    private class BasePlayerSpawnEvent(IPlayer player): PlayerSpawnEvent
+    {
+        public override IPlayer Player => player;
     }
 }
