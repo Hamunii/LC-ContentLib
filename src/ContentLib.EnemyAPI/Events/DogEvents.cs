b30c0504aba672d0133f998755d0691118c9af20
ChroniclerDelta <TheChroniclerDelta@outlook.com>
2024-11-11 13:40:16 -0600

    New Eyeless Dog Events - New DogEvents.cs, containing abstract forms of events related to Eyeless Dogs and their behavior - Small tweaks within EyelessDogPatches.cs - Refactor of ReactToOtherDogHowl(Vector3) into ReactToDogAlert(Vector3)

---
+++ src/ContentLib.EnemyAPI/Events/DogEvents.cs
@@ -0,0 +1,32 @@
+﻿using ContentLib.API.Model.Entity.Enemy.Vanilla.EyelessDog;
+using ContentLib.Core.Model.Event;
+using ContentLib.EnemyAPI.Model.Enemy;
+using UnityEngine;
+
+namespace ContentLib.EnemyAPI.Events;
+
+public abstract class DogHearsNoiseEvent : IMonsterEvents
+{
+    public bool IsCancelled { get; set; }
+    public IEnemy Enemy { get; }
+    public Vector3 NoisePosition { get; }
+    public float NoiseLoudness { get; }
+    public int TimesNoisePlayedInOneSpot { get; }
+    public int NoiseID { get; }
+}
+
+public abstract class DogHearsAlertEvent : IMonsterEvents
+{
+    public bool IsCancelled { get; set; }
+    public IEnemy Enemy { get; }
+    public Vector3 AlertPosition { get; }
+    public IEyelessDog AlertingDog { get; }
+}
+
+public abstract class DogLungeEvent : IMonsterEvents
+{
+    public bool IsCancelled { get; set; }
+    public IEnemy Enemy { get; }
+    public Quaternion LungeVector { get; }
+}
+
