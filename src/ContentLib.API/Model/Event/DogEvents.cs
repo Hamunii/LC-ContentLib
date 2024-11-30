d4d37f02e007151dc886dda45808228c82346c61
ChroniclerDelta <TheChroniclerDelta@outlook.com>
2024-11-30 12:08:02 -0600

    Finalization of Documentation - The last few bits of EyelessDogPatches.cs and DogEvents.cs have been documented - Other than maybe a bit more testing, this branch is ready for Pull Request

--- src/ContentLib.API/Model/Event/DogEvents.cs
+++ src/ContentLib.API/Model/Event/DogEvents.cs
@@ -13,9 +13,21 @@ public abstract class DogHearsNoiseEvent : IMonsterEvents
 {
     public abstract bool IsCancelled { get; set; }
     public abstract IEnemy Enemy { get; }
+    /// <summary>
+    /// The Vector3 position of the Noise the Eyeless Dog heard
+    /// </summary>
     public abstract Vector3 NoisePosition { get; }
+    /// <summary>
+    /// Float representing the Loudness of the noise the Eyeless Dog heard
+    /// </summary>
     public abstract float NoiseLoudness { get; }
+    /// <summary>
+    /// Integer representing the amount of times a noise played in the same spot
+    /// </summary>
     public abstract int TimesNoisePlayedInOneSpot { get; }
+    /// <summary>
+    /// The Integer ID of the noise the Eyeless Dog heard
+    /// </summary>
     public abstract int NoiseID { get; }
 }
 
@@ -26,6 +38,9 @@ public abstract class DogAlertEvent : IMonsterEvents
 {
     public abstract bool IsCancelled { get; set; }
     public abstract IEnemy Enemy { get; }
+    /// <summary>
+    /// The Vector3 position where other Eyeless Dogs will head toward
+    /// </summary>
     public abstract Vector3 AlertPosition { get; }
 }
 
@@ -34,7 +49,9 @@ public abstract class DogAlertEvent : IMonsterEvents
 /// </summary>
 public abstract class DogLungeEvent : IMonsterEvents
 {
-    
+    /// <summary>
+    /// The Quaternion vector this EyelessDog is lunging toward
+    /// </summary>
     public abstract Quaternion LungeVector { get; }
     public abstract bool IsCancelled { get; set; }
     public abstract IEnemy Enemy { get; }
--- src/ContentLib.EnemyAPI/Patches/EyelessDogPatches.cs
+++ src/ContentLib.EnemyAPI/Patches/EyelessDogPatches.cs
@@ -8,8 +8,14 @@ using UnityEngine;
 
 namespace ContentLib.EnemyAPI.Patches;
 
+/// <summary>
+/// Class for patching the game with Eyeless Dog-related events and interactions.
+/// </summary>
 public class EyelessDogPatches
 {
+    /// <summary>
+    /// Method to begin patching the game with Eyeless Dog related events and interactions
+    /// </summary>
     public static void Init()
     {
         CLLogger.Instance.Log("Eyeless Dog being Patched");
@@ -88,32 +94,104 @@ public class EyelessDogPatches
     /// <param name="mouthDogAI">The in-game reference to the Eyeless Dog</param>
     private class LocalEyelessDog(MouthDogAI mouthDogAI): IEyelessDog
     {
+        /// <summary>
+        /// The Network ID for this Eyeless Dog
+        /// </summary>
         public ulong Id => mouthDogAI.NetworkObjectId;
+        /// <summary>
+        /// Boolean representing whether this Eyeless Dog is Alive
+        /// </summary>
         public bool IsAlive => !mouthDogAI.isEnemyDead;
+        /// <summary>
+        /// Integer representing the health of this Eyeless Dog
+        /// </summary>
         public int Health => mouthDogAI.enemyHP;
+        /// <summary>
+        /// The Vector 3 Position of this Eyeless Dog
+        /// </summary>
         public Vector3 Position => mouthDogAI.gameObject.transform.position;
         // TODO: Left empty for now
+        /// <summary>
+        /// The Properties of this Eyeless Dog
+        /// </summary>
         public IEnemyProperties EnemyProperties { get; }
+        /// <summary>
+        /// Boolean representing whether this Eyeless Dog has spawned
+        /// </summary>
         public bool IsSpawned => mouthDogAI.IsSpawned;
+        /// <summary>
+        /// Boolean representing if this Entity is hostile (always True for Eyeless Dogs)
+        /// </summary>
         public bool IsHostile => true;
+        /// <summary>
+        /// Boolean representing if this Eyeless Dog has entered Chase Mode
+        /// </summary>
         public bool IsChasing => mouthDogAI.hasEnteredChaseModeFully;
+        /// <summary>
+        /// Method for immediately killing this Eyeless Dog
+        /// </summary>
         public void Kill() => mouthDogAI.KillEnemy();
+        /// <summary>
+        /// Boolean representing if this Eyeless Dog is Lunging
+        /// </summary>
         public bool isLunging => mouthDogAI.inLunge;
+        /// <summary>
+        /// Integer representing this Eyeless Dog's current Suspicion Level
+        /// </summary>
         public int SuspicionLevel
         {
             get => mouthDogAI.suspicionLevel;
             set => mouthDogAI.suspicionLevel = value;
         }
+        /// <summary>
+        /// Vector3 Position representing the position this Eyeless Dog will head to when hearing a sound
+        /// </summary>
         public Vector3 GuessedSearchPosition => mouthDogAI.noisePositionGuess;
+        /// <summary>
+        /// Vector3 Position representing the true position the sound this Eyeless Dog heard originated from
+        /// </summary>
         public Vector3 AbsoluteSearchPosition => mouthDogAI.lastHeardNoisePosition;
+        /// <summary>
+        /// Method that causes this Eyeless Dog to Lunge toward its current heading
+        /// </summary>
         public void Lunge() => mouthDogAI.EnterLunge();
+        /// <summary>
+        /// Method that causes this Eyeless Dog to grab a given Dead Body into its mouth, used when it kills a player
+        /// </summary>
+        /// <param name="body">A dead Player body</param>
         public void GrabDeadBody(DeadBodyInfo body) => mouthDogAI.TakeBodyInMouth(body);
+        /// <summary>
+        /// Method that causes this Eyeless Dog to drop any dead body it's holding in its mouth
+        /// </summary>
         public void DropDeadBody() => mouthDogAI.DropCarriedBody();
+        /// <summary>
+        /// Method that causes this Eyeless Dog to howl into the air, calling all other Eyeless Dogs to their location
+        /// </summary>
         public void AlertOtherDogs() => mouthDogAI.CallAllDogsWithHowl();
+        /// <summary>
+        /// Method that causes this Eyeless Dog to "hear" a sound with given parameters
+        /// </summary>
+        /// <param name="noisePosition">The Vector3 position of the sound</param>
+        /// <param name="noiseLoudness">Float representing the loudness of the sound</param>
+        /// <param name="timesNoisePlayedInOneSpot">Integer for how many times this sound was played in the same spot</param>
+        /// <param name="noiseID">Integer representing the ID of the sound</param>
         public void DetectNoise(Vector3 noisePosition, float noiseLoudness, int timesNoisePlayedInOneSpot = 0, int noiseID = 0) => mouthDogAI.DetectNoise(noisePosition, noiseLoudness, timesNoisePlayedInOneSpot, noiseID);
-
+        
+        /// <summary>
+        /// Method that causes this Eyeless Dog to immediately head to the given location as if it heard another Eyeless Dog howl
+        /// </summary>
+        /// <param name="howlPosition">The Vector3 position of the other Eyeless Dog's howl</param>
         public void ReactToDogAlert(Vector3 howlPosition) => mouthDogAI.ReactToOtherDogHowl(howlPosition);
-
+        
+        // TODO: Fix discrepancy as Enemies have their own Kill method
+        /// <summary>
+        /// Kills this entity as if it were a player (this method is unnecessary and is due for fixing).
+        /// </summary>
+        /// <param name="bodyVelocity"></param>
+        /// <param name="spawnBody"></param>
+        /// <param name="causeOfDeath"></param>
+        /// <param name="deathAnimation"></param>
+        /// <param name="positionOffset"></param>
         public void Kill(Vector3 bodyVelocity, bool spawnBody = true, CauseOfDeath causeOfDeath = CauseOfDeath.Unknown,
             int deathAnimation = 0, Vector3 positionOffset = default(Vector3)) =>
             throw new System.NotImplementedException();
