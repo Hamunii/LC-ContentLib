b77c20c10ebc0839e8cd8fd952dcf0c1303591e8
RazielMartelus96 <razielmartelus@curiosity-core.com>
2024-11-30 17:58:12 +0000

    - Completed SettingPage.cs documentation. - Refactored some param names, to be more intuitive. - Completed SettingSelectionMenu.cs documentation.

--- src/ContentLib.Core/Model/Terminal/SettingPage.cs
+++ src/ContentLib.Core/Model/Terminal/SettingPage.cs
@@ -6,6 +6,10 @@ using InteractiveTerminalAPI.UI.Screen;
 
 namespace ContentLib.Core.Model.Terminal;
 
+/// <summary>
+/// A specialised page for the InteractiveTerminalAPI that is responsible for showing the Setting Menu for a specified
+/// Config file value.
+/// </summary>
 public class SettingPage : BoxedScreen
 {
     public SettingPage(ConfigKey configKey, Action switchAction, Action updateText)
