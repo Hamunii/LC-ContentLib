f5fdd8e55c686631b586af32675eb12f40479bd6
RazielMartelus96 <razielmartelus@curiosity-core.com>
2024-12-01 04:27:23 +0000

    - Further populated InvalidEventSubscriptionException.cs
    - Added TODO to investigate a util class for the GetFailedInstancePropertiesStatus method...

--- src/ContentLib.API/Exceptions/Core/Events/InvalidEventSubscriptionException.cs
+++ src/ContentLib.API/Exceptions/Core/Events/InvalidEventSubscriptionException.cs
@@ -1,11 +1,35 @@
 using System;
+using ContentLib.API.Model.Event;
 
 namespace ContentLib.API.Exceptions.Core.Events;
 /// <summary>
 /// 
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class InvalidEventSubscriptionException<T> : Exception
+public class InvalidEventSubscriptionException<T>(T invalidEvent, Exception exception) : Exception where T : IGameEvent
 {
-    public override string Message => "Invalid event subscription for: ";
+    private readonly T _invalidEvent = invalidEvent;
+    public T InvalidEvent => _invalidEvent;
+    //TODO Make a util class for this as its likely gonna be used a lot. 
+    public string GetFailedInstancePropertiesStatus()
+    {
+        var properties = typeof(T).GetProperties();
+        var result = "";
+
+        foreach (var property in properties)
+        {
+            try
+            {
+                var value = property.GetValue(InvalidEvent);
+                result += $"{property.Name}: {value}, ";
+            }
+            catch
+            {
+                result += $"{property.Name}: THREW EXCEPTION, ";
+            }
+        }
+
+        return result.TrimEnd(' ', ',');
+    }
+    public override string Message => $"Subscription of {_invalidEvent.GetType().Name} failed!";
 }
