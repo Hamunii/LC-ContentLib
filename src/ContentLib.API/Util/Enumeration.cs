e60ba0d1c1d1b89367709ea500f0ca3ff30b0779
RazielMartelus96 <razielmartelus@curiosity-core.com>
2024-12-06 15:55:48 +0000

    - Refactored Enumeration.cs to allow generic values. - Next commit will focus on documentation of it. And ensuring propper credit to the original developer (found it on a forum but it has an official name)

--- src/ContentLib.API/Util/Enumeration.cs
+++ src/ContentLib.API/Util/Enumeration.cs
@@ -5,22 +5,22 @@ using System.Reflection;
 
 namespace ContentLib.API.Util;
 
-public abstract class Enumeration : IComparable
+public abstract class Enumeration<TValue> : IComparable where TValue : IComparable
 {
-    private readonly int _value;
+    private readonly TValue _value;
     private readonly string _displayName;
 
     protected Enumeration()
     {
     }
 
-    protected Enumeration(int value, string displayName)
+    protected Enumeration(TValue value, string displayName)
     {
         _value = value;
         _displayName = displayName;
     }
 
-    public int Value
+    public TValue Value
     {
         get { return _value; }
     }
@@ -35,7 +35,7 @@ public abstract class Enumeration : IComparable
         return DisplayName;
     }
 
-    public static IEnumerable<T> GetAll<T>() where T : Enumeration, new()
+    public static IEnumerable<T> GetAll<T>() where T : Enumeration<TValue>, new()
     {
         var type = typeof(T);
         var fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
@@ -54,7 +54,7 @@ public abstract class Enumeration : IComparable
 
     public override bool Equals(object obj)
     {
-        var otherValue = obj as Enumeration;
+        var otherValue = obj as Enumeration<TValue>;
 
         if (otherValue == null)
         {
@@ -62,35 +62,42 @@ public abstract class Enumeration : IComparable
         }
 
         var typeMatches = GetType().Equals(obj.GetType());
-        var valueMatches = _value.Equals(otherValue.Value);
+        var valueMatches = EqualityComparer<TValue>.Default.Equals(_value, otherValue.Value);
 
         return typeMatches && valueMatches;
     }
 
     public override int GetHashCode()
     {
-        return _value.GetHashCode();
+        return EqualityComparer<TValue>.Default.GetHashCode(_value);
     }
 
-    public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+    public static int AbsoluteDifference(Enumeration<TValue> firstValue, Enumeration<TValue> secondValue)
     {
-        var absoluteDifference = Math.Abs(firstValue.Value - secondValue.Value);
-        return absoluteDifference;
+        if (typeof(TValue) != typeof(int))
+        {
+            throw new InvalidOperationException("AbsoluteDifference is only supported for int-based Enumeration.");
+        }
+
+        var firstInt = Convert.ToInt32(firstValue.Value);
+        var secondInt = Convert.ToInt32(secondValue.Value);
+
+        return Math.Abs(firstInt - secondInt);
     }
 
-    public static T FromValue<T>(int value) where T : Enumeration, new()
+    public static T FromValue<T>(TValue value) where T : Enumeration<TValue>, new()
     {
-        var matchingItem = parse<T, int>(value, "value", item => item.Value == value);
+        var matchingItem = parse<T, TValue>(value, "value", item => EqualityComparer<TValue>.Default.Equals(item.Value, value));
         return matchingItem;
     }
 
-    public static T FromDisplayName<T>(string displayName) where T : Enumeration, new()
+    public static T FromDisplayName<T>(string displayName) where T : Enumeration<TValue>, new()
     {
         var matchingItem = parse<T, string>(displayName, "display name", item => item.DisplayName == displayName);
         return matchingItem;
     }
 
-    private static T parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration, new()
+    private static T parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration<TValue>, new()
     {
         var matchingItem = GetAll<T>().FirstOrDefault(predicate);
 
@@ -105,6 +112,6 @@ public abstract class Enumeration : IComparable
 
     public int CompareTo(object other)
     {
-        return Value.CompareTo(((Enumeration)other).Value);
+        return Value.CompareTo(((Enumeration<TValue>)other).Value);
     }
 }
